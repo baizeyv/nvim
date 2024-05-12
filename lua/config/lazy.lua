ec1c8e6582c68fba9d9bb19b706a26e86075ef77
baizeyv <baizeyv@gmail.com>
2024-05-12 21:47:57 +0800

    default settings

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,11 +1,42 @@
+-- custom keybinds
+local key = {
+    home = { "H" },
+    install = { "T", "t" },
+    update = { "A", "a" },
+    sync = { "S" },
+    clean = { "X", "x" },
+    check = { "C", "c" },
+    log = { "L", "l" },
+    restore = { "R", "r" },
+    profile = { "P" },
+    debug = { "D" },
+    help = { "?" },
+    build = { "nil", "gb" }
+}
+
+local function load_lazy_keymap()
+    local command = require("lazy.view.config").commands
+    for key, value in pairs(key) do
+        for idx, k in ipairs(value) do
+            if idx == 1 and k ~= "nil" then
+                command[key].key = k
+            elseif idx == 2 then
+                command[key].key_plugin = k
+            end
+        end
+    end
+end
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   -- bootstrap lazy.nvim
   -- stylua: ignore
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "git@github.com:folke/lazy.nvim.git", "--branch=stable", lazypath })
 end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
+
+load_lazy_keymap()
 
 require("lazy").setup({
   spec = {
@@ -27,6 +58,18 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
+git = {
+        -- defaults for the `Lazy log` command
+        -- log = { "--since=3 days ago" }, -- show commits from the last 3 days
+        log = { "-8" }, -- show the last 8 commits
+        timeout = 60, -- kill processes that take more than 2 minutes
+        url_format = "git@github.com:%s.git",
+        -- lazy.nvim requires git >=2.19.0. If you really want to use lazy with an older version,
+        -- then set the below to false. This should work, but is NOT supported and will
+        -- increase downloads a lot.
+        filter = true,
+    },
+  lockfile = vim.fn.stdpath("data") .. '/lazy-lock.json',
   install = { colorscheme = { "tokyonight", "habamax" } },
   checker = { enabled = true }, -- automatically check for plugin updates
   performance = {
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,20 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.opt.linespace = 1
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+if not vim.g.vscode then
+  vim.opt.timeoutlen = 500
+end
+
+vim.o.guifont = "RecMonoLinear Nerd Font Mono:h11"
+if vim.g.neovide then
+  vim.g.neovide_transparency = 0.85
+  vim.g.neovide_hide_mouse_when_typing = true
+  vim.g.neovide_fullscreen = true
+  vim.g.neovide_remember_window_size = true
+  vim.g.neovide_profiler = false
+  vim.g.neovide_cursor_vfx_mode = "wireframe"
+end
